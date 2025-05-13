4b853244aa19fafdb098d11831ad5fe45be84128
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2025-05-13 15:56:00 +0700

    create constructor

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -6,3 +6,10 @@ class mahasiswa
 public:
     mahasiswa(int nim, string nama);
 };
+
+mahasiswa::mahasiswa(int nim, string nama)
+{
+    cout << "construktor dengan parameter terpanggil" << endl;
+    cout << "NIM: " << nim << endl;
+    cout << "Nama: " << nama << endl;
+}
